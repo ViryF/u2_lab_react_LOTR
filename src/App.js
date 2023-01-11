db09afd34031e2ad6f3ee1bd4d7febfd241a6b48
ViryF <diana13_2011@hotmail.com>
2023-01-10 20:05:52 -0600

    changed data to an array of objects

--- src/App.js
+++ src/App.js
@@ -2,29 +2,53 @@ import './App.css'
 import Movie from './components/movie'
 
 const App = () => {
-  const fellowship = {
-    title: 'The Fellowship of the Ring',
-    hours: 2,
-    minutes: 58,
-    poster:
-      'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg'
-  }
+  // const fellowship = {
+  //   title: 'The Fellowship of the Ring',
+  //   hours: 2,
+  //   minutes: 58,
+  //   poster:
+  //     'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg'
+  // }
 
-  const towers = {
-    title: 'The Two Towers',
-    hours: 2,
-    minutes: 59,
-    poster:
-      'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg'
-  }
+  // const towers = {
+  //   title: 'The Two Towers',
+  //   hours: 2,
+  //   minutes: 59,
+  //   poster:
+  //     'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg'
+  // }
 
-  const king = {
-    title: 'The Return of the King',
-    hours: 3,
-    minutes: 21,
-    poster:
-      'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'
-  }
+  // const king = {
+  //   title: 'The Return of the King',
+  //   hours: 3,
+  //   minutes: 21,
+  //   poster:
+  //     'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'
+  // }
+
+  const movies = [
+    {
+      title: 'The Fellowship of the Ring',
+      hours: 2,
+      minutes: 58,
+      poster:
+        'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg'
+    },
+    {
+      title: 'The Two Towers',
+      hours: 2,
+      minutes: 59,
+      poster:
+        'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg'
+    },
+    {
+      title: 'The Return of the King',
+      hours: 3,
+      minutes: 21,
+      poster:
+        'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'
+    }
+  ]
 
   return (
     <div className="App">
@@ -32,7 +56,8 @@ const App = () => {
         <h1>Lord of the Rings</h1>
       </header>
       <main>
-        <Movie fellowship={fellowship} towers={towers} king={king} />
+        <Movie movies={movies} />
+        {/* <Movie fellowship={fellowship} towers={towers} king={king} /> */}
       </main>
     </div>
   )
--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,16 +1,26 @@
-const Movie = ({fellowship, towers, king}) => {
+// const Movie = ({fellowship, towers, king}) => {
+const Movie = ({movies}) => {
 
   return (
     <div>
-      <h1> {fellowship.title}</h1>
+      <h1> {movies[0].title}</h1>
+      <p>Runtime: {movies[0].hours}:{movies[0].minutes} </p>
+      <img src={movies[0].poster} alt="fellowship of the ring" />
+      {/* <h1> {fellowship.title}</h1>
       <p>Runtime: {fellowship.hours}:{fellowship.minutes} </p>
-      <img src={fellowship.poster} alt="fellowship of the ring" />
-      <h1>{towers.title}</h1>
+      <img src={fellowship.poster} alt="fellowship of the ring" /> */}
+      <h1>{movies[1].title}</h1>
+      <p>Runtime: {movies[1].hours}:{movies[1].minutes} </p>
+      <img src={movies[1].poster} alt="the two towers" />
+      {/* <h1>{towers.title}</h1>
       <p>Runtime: {towers.hours}:{towers.minutes} </p>
-      <img src={towers.poster} alt="the two towers" />
-      <h1>{king.title}</h1>
+      <img src={towers.poster} alt="the two towers" /> */}
+      <h1>{movies[2].title}</h1>
+      <p>Runtime: {movies[2].hours}:{movies[2].minutes} </p>
+      <img src={movies[2].poster} alt="return of the king" />
+      {/* <h1>{king.title}</h1>
       <p>Runtime: {king.hours}:{king.minutes} </p>
-      <img src={king.poster} alt="return of the king" />
+      <img src={king.poster} alt="return of the king" /> */}
     </div>
   )
 }
